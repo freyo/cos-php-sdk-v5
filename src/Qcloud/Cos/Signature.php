2dcc8d74748ff862d1ef7bb1292e900a379418d8
freyo <freyhsiao@gmail.com>
2018-05-28 23:43:55 +0800

    fix:signature

--- src/Qcloud/Cos/Signature.php
+++ src/Qcloud/Cos/Signature.php
@@ -4,49 +4,59 @@ namespace Qcloud\Cos;
 
 use Guzzle\Http\Message\RequestInterface;
 
-class Signature {
+class Signature
+{
     private $accessKey;           // string: access key.
     private $secretKey;           // string: secret key.
 
-    public function __construct($accessKey, $secretKey) {
+    public function __construct($accessKey, $secretKey)
+    {
         $this->accessKey = $accessKey;
         $this->secretKey = $secretKey;
     }
 
-    public function __destruct() {
+    public function __destruct()
+    {
     }
 
-    public function signRequest(RequestInterface $request) {
-
-        $signTime = (string)(time() - 60) . ';' . (string)(time() + 3600);
-        $httpString = strtolower($request->getMethod()) . "\n" . urldecode($request->getPath()) .
-            "\n\nhost=" . $request->getHost() . "\n";
-        $sha1edHttpString = sha1($httpString);
-        $stringToSign = "sha1\n$signTime\n$sha1edHttpString\n";
-        $signKey = hash_hmac('sha1', $signTime, $this->secretKey);
-        $signature = hash_hmac('sha1', $stringToSign, $signKey);
-        $authorization = 'q-sign-algorithm=sha1&q-ak='. $this->accessKey .
-            "&q-sign-time=$signTime&q-key-time=$signTime&q-header-list=host&q-url-param-list=&" .
-            "q-signature=$signature";
-
+    public function signRequest(RequestInterface $request)
+    {
+        $signTime      = (time() - 60) . ';' . (time() + 3600);
+        $httpString    = $this->buildHttpString($request);
+        $authorization = urldecode(http_build_query($this->signature($signTime, $httpString)));
         $request->setHeader('Authorization', $authorization);
     }
 
-    public function createPresignedUrl(
-        RequestInterface $request,
-        $expires = "10 minutes"
-    ) {
-        $signTime = (string)(time() - 60) . ';' . (string)(strtotime($expires));
-        $httpString = strtolower($request->getMethod()) . "\n" . urldecode($request->getPath()) .
-            "\n\nhost=" . $request->getHost() . "\n";
-        $sha1edHttpString = sha1($httpString);
-        $stringToSign = "sha1\n$signTime\n$sha1edHttpString\n";
-        $signKey = hash_hmac('sha1', $signTime, $this->secretKey);
-        $signature = hash_hmac('sha1', $stringToSign, $signKey);
-        $authorization = 'q-sign-algorithm=sha1&q-ak='. $this->accessKey .
-            "&q-sign-time=$signTime&q-key-time=$signTime&q-header-list=host&q-url-param-list=&" .
-            "q-signature=$signature";
-        $request->getQuery()->add('sign', $authorization);
+    public function createPresignedUrl(RequestInterface $request, $expires = "10 minutes")
+    {
+        $signTime   = (time() - 60) . ';' . strtotime($expires);
+        $httpString = $this->buildHttpString($request);
+        $request->getQuery()->merge($this->signature($signTime, $httpString));
+
         return $request->getUrl();
+    }
+
+    protected function buildHttpString(RequestInterface $request)
+    {
+        return strtolower($request->getMethod()) . "\n" . urldecode($request->getPath()) .
+            "\n\nhost=" . $request->getHost() . "\n";
+    }
+
+    public function signature($signTime, $httpString)
+    {
+        $sha1edHttpString = sha1($httpString);
+        $stringToSign     = "sha1\n$signTime\n$sha1edHttpString\n";
+        $signKey          = hash_hmac('sha1', $signTime, $this->secretKey);
+        $signature        = hash_hmac('sha1', $stringToSign, $signKey);
+
+        return array(
+            'q-sign-algorithm' => 'sha1',
+            'q-ak'             => $this->accessKey,
+            'q-sign-time'      => $signTime,
+            'q-key-time'       => $signTime,
+            'q-header-list'    => 'host',
+            'q-url-param-list' => '',
+            'q-signature'      => $signature,
+        );
     }
 }
